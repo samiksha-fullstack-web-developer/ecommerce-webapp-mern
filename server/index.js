ab5c65cd672900e6758ff632ec4e68a8785b8843
Samiksha <ssamiksha955@gmail.com>
2025-07-25 21:11:43 +0530

    commit remove

--- server/index.js
+++ server/index.js
@@ -20,13 +20,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // CORS middleware to allow frontend communication
-// app.use(cors({
-//   origin: 'http://localhost:5173',  // Frontend URL
-//   credentials: true,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type']
-// }));
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',  // Frontend URL
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type']
+}));
 
 app.use(cookieParser());
 
